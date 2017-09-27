d4660ec5a886119c985123842dfc9fcaa4b30520
Adam Syed <asyed@e1z2r5p25.42.us.org>
2017-09-27 12:37:48 -0700

    Allocate structures of smaller grids which we can start analyzing

--- file.c
+++ file.c
@@ -6,14 +6,18 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 11:42:03 by asyed             #+#    #+#             */
-/*   Updated: 2017/09/27 12:04:51 by asyed            ###   ########.fr       */
+/*   Updated: 2017/09/27 12:37:05 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
+
+t_grid	*grids;
 
 /*
 ** Lets have a seperate function to read the file from, this is 
@@ -23,12 +27,34 @@ void	file_reader(char *filename)
 {
 	int		fd;
 	int		r;
+	int		small_grids;
 	char	buf[BUF_SIZE + 1];
 
 	fd = open(filename, O_RDONLY);
 	while ((r = read(fd, buf, BUF_SIZE)))
 	{
 		buf[r] = '\0';
+		printf("%s\n", buf);
 		//Process data in buffer.
 	}
+	small_grids = numofgrids(buf);
+	// grids = (t_grid *)ft_memalloc(small_grids * sizeof(struct s_grid *));
+	grids = (t_grid *)calloc(small_grids, sizeof(struct s_grid *));
+	if (!grids)
+	{
+		ft_putstr("Error in allocating small grids!");
+		exit(2);
+	}
+	printf("We malloc'd %d grids for a size of %lu bytes\n", small_grids, small_grids * sizeof(struct s_grid *));
+}
+
+int		numofgrids(char	*buf)
+{
+	int	i;
+
+	i = 1;
+	while (*buf && *buf != '\n')
+		if (*buf++ == ' ')
+			i++;
+	return (i);
 }
--- fillit.h
+++ fillit.h
@@ -6,21 +6,28 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 11:43:42 by asyed             #+#    #+#             */
-/*   Updated: 2017/09/27 12:04:42 by asyed            ###   ########.fr       */
+/*   Updated: 2017/09/27 12:34:34 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FILLIT_H
 # define FILLIT_H
 
 # include <unistd.h>
-/*
-** Technically the file should only have 45 bytes if its valid. 
-** We can increase this later to account for errors if needed. 
-*/
-# define BUF_SIZE 45
+// # include "libft.h"
+
+# define BUF_SIZE 2048
+
+typedef	struct	s_grid
+{
+	char	type;
+	char	x;
+	char	y;
+}				t_grid;
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 void	file_reader(char *filename);
+int		numofgrids(char	*buf);
 
+extern	t_grid	*grids;
 #endif
